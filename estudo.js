78e488affb22fc4266167b9e45c2f2eadd5dab6a
raphael.teixeira <raphael.cazuza@gmail.com>
2017-12-27 22:07:19 -0200

    commit loop lesson

--- estudo.js
+++ estudo.js
@@ -1,3 +1,40 @@
+var numero = 100;
+
+if (numero === "100") {
+    console.log("It is equal");
+} else {
+    console.log("It is not equal");
+}
+
+var fruits = [
+    { name: "apple" },
+    { name: "orange" },
+    { name: "melon" },
+    { name: "strawberry" }
+];
+
+for (var fruit in fruits) {
+    if (fruits[fruit].name == "orange") {
+        console.log("I love " + fruits[fruit].name + "!");
+    } else {
+        var x;
+
+        if (isVowel(fruits[fruit].name.charAt(0))) {
+            x = "an";
+            console.log("This is not an orange. This is " + x + " " + fruits[fruit].name);
+        } else {
+            x = "a";
+            console.log("This is not an orange. This is " + x + " " + fruits[fruit].name);
+        }
+    }
+}
+
+function isVowel(vowel) {
+    var result = vowel == "a" || vowel == "e" || vowel == "i" || vowel == "o" || vowel == "u" || vowel == "A" || vowel == "E" || vowel == "I" || vowel == "O" || vowel == "U";
+
+    return result;
+}
+
 var carro = {
     velocidade: 200,
     marca: "volvo",
@@ -44,4 +81,19 @@ function name(fullName) {
     return fullName();
 }
 
-console.log(name(function () { return "Raphael Arcanjo Ewerton Teixeira" }));
+//console.log(name(function () { return "Raphael Arcanjo Ewerton Teixeira" }));
+
+
+function Apple(color, weight) {
+    this.color = color;
+    this.weight = weight;
+}
+
+Apple.prototype = {
+    eat: function () { return this },
+    throw: function () { return "throw an apple" }
+}
+
+var apple1 = new Apple("Red", 99);
+var apple1 = new Apple("Blue", 109);
+var apple1 = new Apple("Black", 209);
